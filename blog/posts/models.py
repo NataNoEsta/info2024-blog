821b16b9996767c36befff13fe270d6bc8a4e9d6
Aldeer <gomilaemanuel@gmail.com>
2024-10-07 22:23:58 -0300

    modelo posts

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    ultima_modificacion = models.DateTimeField(auto_now=True)
+    
