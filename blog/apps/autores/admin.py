49761ed8780962c99cb4c10efcc2c571d4b9d94c
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-09 17:52:10 -0300

    barra de busqueda en admin site y listas

--- blog/apps/autores/admin.py
+++ blog/apps/autores/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Autor
 
-# Register your models here.
-admin.site.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    search_fields = ['nombres']
+    list_display = ('nombres', 'apellidos', 'estado', 'fecha_creacion')
+
+admin.site.register(Autor, AutorAdmin)
