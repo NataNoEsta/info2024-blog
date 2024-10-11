955591c757fc00dd0f860df59098e021471dd026
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-10-11 10:28:59 -0300

    formulario para las publicaciones

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, formulario
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('form_post/', formulario, name='formPost'),
 ]
--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'pagina_principal.html', context=context)
+
+
+def formulario(request):
+    form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'formulario_post.html', context)
